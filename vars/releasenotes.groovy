78e17abda3538b00456873dbfb5039c880ae844d
mgalal2 <mgalal2@its.jnj.com>
2020-05-16 00:05:07 +0100

    Append script

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -4,7 +4,7 @@ import groovy.io.*;
 @NonCPS
 def call(Map config=[:]) {
     def workspace = new File(pwd()) + "@script";
-    new File(workspace.path + 'manifest.txt').withWriter('utf-8')
+    new File(workspace.path + '/manifest.txt').withWriter('utf-8')
             {
                 writer ->
                     workspace.eachFileRecurse(FileType.ANY) {
