24bc4dcf392f74f4a128e051c01fa3a2317881bd
mgalal2 <mgalal2@its.jnj.com>
2020-05-24 02:09:05 +0100

    Log the change sets

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,5 +1,7 @@
-import java.io.*
+import hudson.model.*;
+import hudson.scm.*;
 import groovy.io.*
+
 
 import java.text.SimpleDateFormat;
 
@@ -21,9 +23,22 @@ def call(Map config=[:]) {
     def date =  new Date();
     def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:sss");
     echo "Date and Time IS: " + sdf.format(date);
-
+    currentBuild =  (Build)currentBuild
     echo "Last build number is " + currentBuild.getNumber()
     echo "Current build number ${BUILD_NUMBER}"
+
+    ChangeLogSet changeLogSets = currentBuild.changeSet;
+
+    for(change in changeLogSets) {
+         entries = change.items;
+        for (Entry entry in entries) {
+            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)} ${entry.msg})"
+            for(file in entry.affectedFiles){
+                echo "${file.editType.name} ${file.path}"
+            }
+        }
+    }
+
     if(config.changes != "false") {
         echo "changes";
     }
